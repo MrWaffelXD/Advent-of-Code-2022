baf8ee4cd367cfc4fee264ffb6b469f74ef2bfb4
MrWaffelXD <accounts@jonas-heinze.de>
2022-12-01 13:32:22 +0100

    day 1

--- Day 1/Day 1/Program.cs
+++ Day 1/Day 1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Day_1
 {
@@ -6,7 +7,61 @@ namespace Day_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            task2();
+        }
+        static void task1()
+        {
+            string input = System.IO.File.ReadAllText("input.txt");
+            string[] elves = input.Split("\r\n\r\n");
+
+            List<int> totalCaloriesList = new List<int>();
+
+            foreach(string elf in elves)
+            {
+                string[] rations = elf.Split("\r\n");
+
+                int totalCalories = 0;
+
+                foreach(string ration in rations)
+                {
+                    if(ration != "")
+                    {
+                        totalCalories += Convert.ToInt32(ration);
+                    }
+                }
+                totalCaloriesList.Add(totalCalories);
+            }
+            totalCaloriesList.Sort();
+            totalCaloriesList.Reverse();
+            Console.WriteLine(totalCaloriesList[0].ToString());
+        }
+        static void task2()
+        {
+            string input = System.IO.File.ReadAllText("input.txt");
+            string[] elves = input.Split("\r\n\r\n");
+
+            List<int> totalCaloriesList = new List<int>();
+
+            foreach (string elf in elves)
+            {
+                string[] rations = elf.Split("\r\n");
+
+                int totalCalories = 0;
+
+                foreach (string ration in rations)
+                {
+                    if (ration != "")
+                    {
+                        totalCalories += Convert.ToInt32(ration);
+                    }
+                }
+                totalCaloriesList.Add(totalCalories);
+            }
+            totalCaloriesList.Sort();
+            totalCaloriesList.Reverse();
+
+            int totalTopThree = totalCaloriesList[0] + totalCaloriesList[1] + totalCaloriesList[2];
+            Console.WriteLine(totalTopThree.ToString());
         }
     }
 }
