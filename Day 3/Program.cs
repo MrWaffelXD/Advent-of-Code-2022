5d73fcc01ff02a1b4408e782a08e7b7f040a20c9
MrWaffelXD <accounts@jonas-heinze.de>
2022-12-04 20:24:50 +0100

    day 3 task 1

--- Day 3/Program.cs
+++ Day 3/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace Day_3
 {
@@ -6,7 +7,76 @@ namespace Day_3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] input = File.ReadAllLines("input.txt");
+            Console.WriteLine(task2(input));
+        }
+        static int task1(string[] input)
+        {
+            int totalPrio = 0;
+
+            foreach (string bp in input)
+            {
+                string[] compartments = new string[2];
+                compartments[0] = bp.Substring(0, bp.Length / 2);
+                compartments[1] = bp.Substring(bp.Length / 2, bp.Length / 2);
+
+                char doubleItemName;
+                int doubleItemValue = 0;
+                
+                foreach(char c in compartments[0])
+                {
+                    if (compartments[1].Contains(c))
+                    {
+                        doubleItemName = c;
+                        if (Char.IsUpper(c))
+                        {
+                            doubleItemValue = c - 38;
+                        }
+                        else
+                        {
+                            doubleItemValue = c - 96;
+                        }
+                        
+                    }
+                }
+                totalPrio += doubleItemValue;
+                
+            }
+            return totalPrio;
+        }
+        static int task2(string[] input)
+        {
+            int totalPrio = 0;
+
+            foreach (string bp in input)
+            {
+                string[] compartments = new string[2];
+                compartments[0] = bp.Substring(0, bp.Length / 2);
+                compartments[1] = bp.Substring(bp.Length / 2, bp.Length / 2);
+
+                char doubleItemName;
+                int doubleItemValue = 0;
+
+                foreach (char c in compartments[0])
+                {
+                    if (compartments[1].Contains(c))
+                    {
+                        doubleItemName = c;
+                        if (Char.IsUpper(c))
+                        {
+                            doubleItemValue = c - 38;
+                        }
+                        else
+                        {
+                            doubleItemValue = c - 96;
+                        }
+
+                    }
+                }
+                totalPrio += doubleItemValue;
+
+            }
+            return totalPrio;
         }
     }
 }
