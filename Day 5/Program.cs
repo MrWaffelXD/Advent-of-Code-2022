ecb69beab34a8a6ff280c15a9d27bf0adc1ac76e
MrWaffelXD <accounts@jonas-heinze.de>
2022-12-05 22:41:54 +0100

    day 5 init

---
+++ Day 5/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.IO;
+
+namespace Day_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = File.ReadAllText("input.txt");
+        }
+    }
+}
