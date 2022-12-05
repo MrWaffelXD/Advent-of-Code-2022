fc9879028d42129fff84503305f369db881c7e96
MrWaffelXD <accounts@jonas-heinze.de>
2022-12-05 22:35:22 +0100

    day 4 task 2

--- Day 4/Program.cs
+++ Day 4/Program.cs
@@ -9,7 +9,7 @@ namespace Day_4
         static void Main(string[] args)
         {
             string[] input = File.ReadAllLines("input.txt");
-            Console.WriteLine(task1(input));
+            Console.WriteLine(task2(input));
         }
         static int task1(string[] input)
         {
@@ -48,6 +48,49 @@ namespace Day_4
                 }
 
                 if(foundCounter == pair.Key.Count || foundCounter == pair.Value.Count)
+                {
+                    doublePairCounter++;
+                }
+            }
+            return doublePairCounter;
+        }
+        static int task2(string[] input)
+        {
+            int doublePairCounter = 0;
+            Dictionary<List<int>, List<int>> pairAreaListFull = new Dictionary<List<int>, List<int>>();
+            foreach (string pairString in input)
+            {
+                string[] pair = pairString.Split(",");
+
+                List<List<int>> pairAreas = new List<List<int>>();
+
+                foreach (string p in pair)
+                {
+                    string[] areaLimits = p.Split("-");
+                    List<int> areas = new List<int>();
+
+                    for (int i = Convert.ToInt32(areaLimits[0]); i <= Convert.ToInt32(areaLimits[1]); i++)
+                    {
+                        areas.Add(i);
+                    }
+
+                    pairAreas.Add(areas);
+                }
+                pairAreaListFull.Add(pairAreas[0], pairAreas[1]);
+            }
+
+            foreach (KeyValuePair<List<int>, List<int>> pair in pairAreaListFull)
+            {
+                int foundCounter = 0;
+                foreach (int area in pair.Key)
+                {
+                    if (pair.Value.Contains(area))
+                    {
+                        foundCounter++;
+                    }
+                }
+
+                if (foundCounter > 0)
                 {
                     doublePairCounter++;
                 }
